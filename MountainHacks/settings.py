fe870a15bcfc41d92fe9369039f3b760d356a73a
Derek Argueta <darguetap@gmail.com>
2014-10-24 12:27:26 -0600

    heroku settings

--- MountainHacks/settings.py
+++ MountainHacks/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'log_request_id.middleware.RequestIDMiddleware',
+    #'log_request_id.middleware.RequestIDMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -72,35 +72,35 @@ STATIC_URL = '/static/'
 # Support for X-Request-ID
 # https://devcenter.heroku.com/articles/http-request-id-staging
 
-LOG_REQUEST_ID_HEADER = 'HTTP_X_REQUEST_ID'
-LOG_REQUESTS = True
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'request_id': {
-            '()': 'log_request_id.filters.RequestIDFilter'
-        }
-    },
-    'formatters': {
-        'standard': {
-            'format': '%(levelname)-8s [%(asctime)s] [%(request_id)s] %(name)s: %(message)s'
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'filters': ['request_id'],
-            'formatter': 'standard',
-        },
-    },
-    'loggers': {
-        'log_request_id.middleware': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    }
-}
+# LOG_REQUEST_ID_HEADER = 'HTTP_X_REQUEST_ID'
+# LOG_REQUESTS = True
+#
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'filters': {
+#         'request_id': {
+#             '()': 'log_request_id.filters.RequestIDFilter'
+#         }
+#     },
+#     'formatters': {
+#         'standard': {
+#             'format': '%(levelname)-8s [%(asctime)s] [%(request_id)s] %(name)s: %(message)s'
+#         },
+#     },
+#     'handlers': {
+#         'console': {
+#             'level': 'DEBUG',
+#             'class': 'logging.StreamHandler',
+#             'filters': ['request_id'],
+#             'formatter': 'standard',
+#         },
+#     },
+#     'loggers': {
+#         'log_request_id.middleware': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#             'propagate': False,
+#         },
+#     }
+# }
