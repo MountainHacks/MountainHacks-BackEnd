508dcf8df77676c51168b1d7c6a683de12c915cd
Derek Argueta <darguetap@gmail.com>
2014-10-24 11:32:49 -0600

    gitignore

---
+++ MountainHacks/secrets.py
@@ -0,0 +1,4 @@
+__author__ = 'Derek Argueta'
+__email__  = 'derek@mountainhacks.com'
+
+SECRET_KEY = 'o=@9zxh%t7%7&p@7k0a1(#expete%@b0jy+k!^o3!7i$x0jax*'
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn MountainHacks.wsgi 
