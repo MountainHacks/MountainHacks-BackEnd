9f6406f81328a7a43a027f6a99dcab3f20b0612c
Derek Argueta <darguetap@gmail.com>
2014-10-24 11:54:49 -0600

    postgres for heroku

--- MountainHacks/settings.py
+++ MountainHacks/settings.py
@@ -3,13 +3,18 @@ __email__  = 'derek@mountainhacks.com'
 
 import os
 from MountainHacks import secrets
+import dj_database_url
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = secrets.SECRET_KEY
 DEBUG = True
 TEMPLATE_DEBUG = True
-ALLOWED_HOSTS = []
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,16 +44,24 @@ ROOT_URLCONF = 'MountainHacks.urls'
 
 WSGI_APPLICATION = 'MountainHacks.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config()
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'US/Mountain'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
