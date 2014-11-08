06e7fffac9f6261925918a9ecdd63a1cd49dc06e
Derek Argueta <darguetap@gmail.com>
2014-11-08 03:46:53 -0700

    Added Student model to Django admin

--- MountainHacks/settings.py
+++ MountainHacks/settings.py
@@ -3,7 +3,6 @@ __email__  = 'derek@mountainhacks.com'
 
 import os
 from MountainHacks import secrets
-import dj_database_url
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -83,41 +82,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-
-
-
-# Support for X-Request-ID
-# https://devcenter.heroku.com/articles/http-request-id-staging
-
-# LOG_REQUEST_ID_HEADER = 'HTTP_X_REQUEST_ID'
-# LOG_REQUESTS = True
-#
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'filters': {
-#         'request_id': {
-#             '()': 'log_request_id.filters.RequestIDFilter'
-#         }
-#     },
-#     'formatters': {
-#         'standard': {
-#             'format': '%(levelname)-8s [%(asctime)s] [%(request_id)s] %(name)s: %(message)s'
-#         },
-#     },
-#     'handlers': {
-#         'console': {
-#             'level': 'DEBUG',
-#             'class': 'logging.StreamHandler',
-#             'filters': ['request_id'],
-#             'formatter': 'standard',
-#         },
-#     },
-#     'loggers': {
-#         'log_request_id.middleware'
 STATIC_ROOT = '/home/darguetap/webapps/mountainhacks_static/'
 STATIC_URL = '/static/'
 
--- api/admin.py
+++ api/admin.py
@@ -2,5 +2,7 @@ __author__ = 'Derek Argueta'
 __email__  = 'derek@mountainhacks.com'
 
 from django.contrib import admin
+from api.models import Student
 
 # Register your models here.
+admin.site.register(Student)
