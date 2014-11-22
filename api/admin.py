d0aa361a6540a07d27b9ae760e55f391d448d68a
Derek Argueta <darguetap@gmail.com>
2014-11-22 10:28:20 -0700

    added bootstrap to admin and ability to filter data

--- api/admin.py
+++ api/admin.py
@@ -7,6 +7,7 @@ from api.models import Student, Company#, SessionToken
 class StudentAdmin(admin.ModelAdmin):
     readonly_fields = ('registration_date', 'is_confirmed')
     exclude = ('confirmation_code',)
+    list_filter = ('is_confirmed', 'gender', 'school', 'grade', 'shirt_size', 'out_of_state')
 
 # Register your models here.
 admin.site.register(Student, StudentAdmin)
